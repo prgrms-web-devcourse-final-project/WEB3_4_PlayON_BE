90efb00571f779a81405852a03c09c0a143f2ea8
wjwj910 <wjwj910@gmail.com>
2025-03-31 17:20:39 +0900

    [FEAT] 알림 엔티티 추가
    * ERD를 기반으로 한 엔티티 생성
    * 추후에 엔티티 필드 및 Type 수정 할 예정

---
+++ src/main/java/com/ll/playon/domain/notification/entity/Notification.java
@@ -0,0 +1,34 @@
+package com.ll.playon.domain.notification.entity;
+
+import com.ll.playon.domain.member.entity.Member;
+import com.ll.playon.global.jpa.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Notification extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "receiver_id", nullable = false)
+    private Member receiver;
+
+    @Column(nullable = false, length = 255)
+    private String content;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private NotificationType type;
+
+    @Column(nullable = false)
+    @Builder.Default
+    private boolean isRead = false;
+
+    private String redirectUrl;
+}
---
+++ src/main/java/com/ll/playon/domain/notification/entity/NotificationType.java
@@ -0,0 +1,7 @@
+package com.ll.playon.domain.notification.entity;
+
+public enum NotificationType {
+    FRIEND_REQUEST,
+    GAME_INVITE,
+    SYSTEM_ALERT
+}
