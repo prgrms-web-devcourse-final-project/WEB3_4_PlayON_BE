324dc75e371ef6ca549dd67f16e370af06caef16
S2hyeyunS2 <hhyeyun22@gmail.com>
2025-03-31 12:43:53 +0900

    [FEAT] GuildJoinRequest ErrorCode 추가

--- src/main/java/com/ll/playon/global/exceptions/ErrorCode.java
+++ src/main/java/com/ll/playon/global/exceptions/ErrorCode.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 
+
 @Getter
 @AllArgsConstructor
 @Slf4j
@@ -21,7 +22,14 @@ public enum ErrorCode {
     EXTERNAL_API_COMMUNICATION_ERROR(HttpStatus.BAD_GATEWAY, "API 요청 중 오류가 발생했습니다."),
 
     // Filtering
-    PAGE_SIZE_LIMIT_EXCEEDED(HttpStatus.BAD_REQUEST, "pageSize는 1에서 100 사이로 입력해주세요.");
+    PAGE_SIZE_LIMIT_EXCEEDED(HttpStatus.BAD_REQUEST, "pageSize는 1에서 100 사이로 입력해주세요."),
+
+    // guildJoinRequest
+    GUILD_ALREADY_REQUESTED(HttpStatus.CONFLICT, "이미 해당 길드에 가입 요청을 보냈습니다."),
+    GUILD_JOIN_REQUEST_NOT_FOUND(HttpStatus.BAD_REQUEST, "가입 요청을 찾을 수 없습니다."),
+    GUILD_ID_MISMATCH(HttpStatus.BAD_REQUEST, "요청한 길드와 일치하지 않습니다."),
+    GUILD_REQUEST_ALREADY_PROCESSED(HttpStatus.BAD_REQUEST, "이미 처리된 길드 요청입니다."),
+    GUILD_APPROVAL_UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "승인 권한이 없습니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
