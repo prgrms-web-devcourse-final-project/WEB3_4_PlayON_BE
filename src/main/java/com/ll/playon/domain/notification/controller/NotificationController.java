d31c0a53a44579024b154d4dbfbd170f36e4a100
wjwj910 <wjwj910@gmail.com>
2025-04-01 12:44:55 +0900

    [FEAT] SSE 연결 컨트롤러 추가

--- src/main/java/com/ll/playon/domain/notification/controller/NotificationController.java
+++ src/main/java/com/ll/playon/domain/notification/controller/NotificationController.java
@@ -1,9 +1,24 @@
 package com.ll.playon.domain.notification.controller;
 
+import com.ll.playon.domain.member.entity.Member;
+import com.ll.playon.domain.notification.service.NotificationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 @RestController
 @RequestMapping("/api/notifications")
+@RequiredArgsConstructor
 public class NotificationController {
+
+    private final NotificationService notificationService;
+
+    @GetMapping(value = "/subscribe", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public SseEmitter subscribe(@AuthenticationPrincipal Member member) {
+        return notificationService.subscribe(member.getId());
+    }
 }
