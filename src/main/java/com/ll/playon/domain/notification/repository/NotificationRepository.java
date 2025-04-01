d1d414fc95415f711ea97ea4d5f7e1967d4fcdbb
wjwj910 <wjwj910@gmail.com>
2025-04-01 10:42:25 +0900

    [FEAT] 알림 리포지토리 인터페이스 추가

---
+++ src/main/java/com/ll/playon/domain/notification/repository/NotificationRepository.java
@@ -0,0 +1,7 @@
+package com.ll.playon.domain.notification.repository;
+
+import com.ll.playon.domain.notification.entity.Notification;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
+}
