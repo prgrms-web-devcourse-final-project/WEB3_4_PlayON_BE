422c80d4ef8a269f0686ffecec4b4eb2149ef6e7
wjwj910 <wjwj910@gmail.com>
2025-04-01 12:46:15 +0900

    [FEAT] 컨트롤러에서 알림 생성 API 추가

--- src/main/java/com/ll/playon/domain/notification/controller/NotificationController.java
+++ src/main/java/com/ll/playon/domain/notification/controller/NotificationController.java
@@ -4,10 +4,9 @@ import com.ll.playon.domain.member.entity.Member;
 import com.ll.playon.domain.notification.service.NotificationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 @RestController
@@ -20,5 +19,12 @@ public class NotificationController {
     @GetMapping(value = "/subscribe", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public SseEmitter subscribe(@AuthenticationPrincipal Member member) {
         return notificationService.subscribe(member.getId());
+    }
+
+    @PostMapping("/send")
+    public ResponseEntity<Void> sendNotification(@RequestParam Long receiverId,
+                                                 @RequestParam String message) {
+        notificationService.sendNotification(receiverId, message);
+        return ResponseEntity.ok().build();
     }
 }
