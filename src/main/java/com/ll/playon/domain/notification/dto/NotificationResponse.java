38f623519ecdded4edaa93cd8912fd0341259620
wjwj910 <wjwj910@gmail.com>
2025-04-02 10:25:31 +0900

    [REFACTOR] dto, record 변환

--- src/main/java/com/ll/playon/domain/notification/dto/NotificationResponse.java
+++ src/main/java/com/ll/playon/domain/notification/dto/NotificationResponse.java
@@ -2,18 +2,14 @@ package com.ll.playon.domain.notification.dto;
 
 import com.ll.playon.domain.notification.entity.Notification;
 import com.ll.playon.domain.notification.entity.NotificationType;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
 
-@Getter
-@AllArgsConstructor
-public class NotificationResponse {
-    private Long id;
-    private String content;
-    private NotificationType type;
-    private boolean read;
-    private String redirectUrl;
-
+public record NotificationResponse(
+        Long id,
+        String content,
+        NotificationType type,
+        boolean read,
+        String redirectUrl
+) {
     public static NotificationResponse fromEntity(Notification notification) {
         return new NotificationResponse(
                 notification.getId(),
