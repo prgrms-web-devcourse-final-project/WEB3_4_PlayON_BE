813225afe3bedbc97d8b8f56b6a4d3a4875bc114
wjwj910 <wjwj910@gmail.com>
2025-04-03 15:50:24 +0900

    [REFACTOR] 불필요 메서드 제거

--- src/main/java/com/ll/playon/domain/notification/dto/request/NotificationRequest.java
+++ src/main/java/com/ll/playon/domain/notification/dto/request/NotificationRequest.java
@@ -3,13 +3,8 @@ package com.ll.playon.domain.notification.dto.request;
 import com.ll.playon.domain.notification.entity.NotificationType;
 
 public record NotificationRequest(
-        Long senderId,
         Long receiverId,
         String content,
         NotificationType type,
         String redirectUrl
-) {
-    public NotificationRequest withSenderId(Long senderId) {
-        return new NotificationRequest(senderId, receiverId, content, type, redirectUrl);
-    }
-}
+) { }
