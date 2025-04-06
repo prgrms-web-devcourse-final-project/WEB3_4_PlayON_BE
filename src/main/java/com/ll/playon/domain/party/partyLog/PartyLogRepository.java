031ebb8498fb76392069e9cefe38efd5e5c2a215
wjwj910 <wjwj910@gmail.com>
2025-04-07 05:57:33 +0900

    [FIX] dev 브랜치에 있는 불필요 PartyLogRepository 제거

--- src/main/java/com/ll/playon/domain/party/partyLog/PartyLogRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ll.playon.domain.party.partyLog;
-
-import com.ll.playon.domain.party.partyLog.entity.PartyLog;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PartyLogRepository extends JpaRepository<PartyLog, Long> {
-}
