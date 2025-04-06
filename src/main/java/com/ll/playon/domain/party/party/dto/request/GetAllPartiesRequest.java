c7807e63f8b4621c76c473b7f11c58d0c7d67ade
Gyeonghun Min <riverpower6@naver.com>
2025-04-07 06:18:40 +0900

    [REFACTOR] 파티 리스트 응답에 headerImage 추가

--- src/main/java/com/ll/playon/domain/party/party/dto/request/GetAllPartiesRequest.java
+++ src/main/java/com/ll/playon/domain/party/party/dto/request/GetAllPartiesRequest.java
@@ -7,21 +7,16 @@ import java.util.List;
 import java.util.Objects;
 
 public record GetAllPartiesRequest(
+        Long gameId,
 
-//        // TODO: 수정할 가능성
-//        @NotNull
-//        String gameName,
-
-        // TODO: 추후 게임 작업 완료되면 진행
-//        @NotNull
-//        List<String> genres,
+        @NotNull
+        List<String> genres,
 
         @NotNull
         List<@Valid PartyTagRequest> tags
 ) {
     public GetAllPartiesRequest {
-//        gameName = Objects.requireNonNullElse(gameName, "");
-//        genres = Objects.requireNonNullElse(genres, new ArrayList<>());
+        genres = Objects.requireNonNullElse(genres, new ArrayList<>());
         tags = Objects.requireNonNullElse(tags, new ArrayList<>());
     }
 }
