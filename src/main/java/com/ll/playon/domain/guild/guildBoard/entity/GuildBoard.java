3acadf59ef58cbf9ebcf4bc18b30150b4772e34f
S2hyeyunS2 <hhyeyun22@gmail.com>
2025-04-07 11:16:56 +0900

    [FEAT] 길드게시판 엔티티 생성

---
+++ src/main/java/com/ll/playon/domain/guild/guildBoard/entity/GuildBoard.java
@@ -0,0 +1,67 @@
+package com.ll.playon.domain.guild.guildBoard.entity;
+
+import com.ll.playon.domain.guild.guild.entity.Guild;
+import com.ll.playon.domain.guild.guildBoard.enums.BoardTag;
+import com.ll.playon.domain.guild.guildMember.entity.GuildMember;
+import com.ll.playon.global.jpa.entity.BaseTime;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class GuildBoard extends BaseTime {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch=FetchType.LAZY)
+    @JoinColumn(name="guild_id", nullable = false)
+    private Guild guild;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "author_id", nullable = false)
+    private GuildMember author;
+
+    @Column(nullable = false, length = 100)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private BoardTag tag;
+
+    @Column(nullable = false)
+    @Builder.Default
+    private int hit=0;
+
+    @Column(nullable = false)
+    @Builder.Default
+    private int likeCount=0;
+
+    private String imageUrl;
+
+    public void increaseHit() {
+        this.hit++;
+    }
+
+    public void increaseLike() {
+        this.likeCount++;
+    }
+
+    public void decreaseLike() {
+        this.likeCount = Math.max(0, this.likeCount - 1);
+    }
+
+    public void update(String title, String content, BoardTag tag, String imageUrl) {
+        this.title = title;
+        this.content = content;
+        this.tag = tag;
+        this.imageUrl = imageUrl;
+    }
+}
---
+++ src/main/java/com/ll/playon/domain/guild/guildBoard/entity/GuildBoardComment.java
@@ -0,0 +1,33 @@
+package com.ll.playon.domain.guild.guildBoard.entity;
+
+import com.ll.playon.domain.guild.guildMember.entity.GuildMember;
+import com.ll.playon.global.jpa.entity.BaseTime;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class GuildBoardComment extends BaseTime {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch=FetchType.LAZY)
+    @JoinColumn(name="board_id", nullable = false)
+    private GuildBoard board;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "author_id", nullable = false)
+    private GuildMember author;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String comment;
+
+    public void update(String newComment) {
+        this.comment = newComment;
+    }
+}
