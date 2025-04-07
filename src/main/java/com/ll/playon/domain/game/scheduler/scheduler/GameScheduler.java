d8e59922a91ee9e2230f8a06bc3f51eb45320f06
Taehee <134465372+Taeheeeeeee@users.noreply.github.com>
2025-04-07 17:01:50 +0900

    [FEAT] 유저가 많이 선택한 인기 게임 조회 (#69)
    * [FEAT] 유저가 많이 선택한 인기 게임 조회
    
    * [REMOVE] 사용하지 않는 서비스 제거
    
    * [REFACTOR] 샘플 데이터 수정

---
+++ src/main/java/com/ll/playon/domain/game/scheduler/scheduler/GameScheduler.java
@@ -0,0 +1,47 @@
+package com.ll.playon.domain.game.scheduler.scheduler;
+
+import com.ll.playon.domain.game.game.entity.WeeklyPopularGame;
+import com.ll.playon.domain.game.scheduler.repository.WeeklyGameRepository;
+import com.ll.playon.domain.party.party.repository.PartyRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
+
+@RequiredArgsConstructor
+@Component
+public class GameScheduler {
+    private final PartyRepository partyRepository;
+    private final WeeklyGameRepository weeklyGameRepository;
+
+    @Scheduled(cron = "0 0 0 * * MON") // 매주 월요일 00:00
+//    @Scheduled(cron = "0 * * * * *")
+    public void updateWeeklyGameStats() {
+        LocalDate weekStart = LocalDate.now().with(DayOfWeek.MONDAY);
+        LocalDateTime fromDate = weekStart.minusWeeks(1).atStartOfDay(); // 지난주
+        LocalDateTime toDate = weekStart.atStartOfDay(); // 이번주
+
+        updatePopularGames(fromDate, toDate, weekStart);
+    }
+
+    public void updatePopularGames(LocalDateTime fromDate, LocalDateTime toDate, LocalDate weekStart) {
+        final int limit = 3;
+
+        List<Map<String, Object>> result = partyRepository.findTopGamesByPartyLastWeek(fromDate, toDate, limit);
+
+        List<WeeklyPopularGame> list = result.stream()
+                .map(row -> WeeklyPopularGame.builder()
+                        .gameId(((Number) row.get("gameId")).longValue())
+                        .playCount(((Number) row.get("playCount")).longValue())
+                        .weekStartDate(weekStart)
+                        .build())
+                .toList();
+
+        weeklyGameRepository.saveAll(list);
+    }
+}
