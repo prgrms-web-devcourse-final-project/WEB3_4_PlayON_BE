f9e29ae7c69423947547701a02aa3558c80fc3c2
Taeheeeeeee <taeheemin62@gmail.com>
2025-04-09 20:14:34 +0900

    [CONFLICT] 병합 충돌 해결

--- src/main/java/com/ll/playon/domain/guild/guild/repository/GuildMemberRepositoryCustomImpl.java
+++ src/main/java/com/ll/playon/domain/guild/guild/repository/GuildMemberRepositoryCustomImpl.java
@@ -4,9 +4,6 @@ import com.ll.playon.domain.guild.guild.entity.Guild;
 import com.ll.playon.domain.guild.guildMember.entity.GuildMember;
 import com.ll.playon.domain.guild.guildMember.entity.QGuildMember;
 import com.ll.playon.domain.guild.guildMember.enums.GuildRole;
-import com.ll.playon.domain.member.entity.QMember;
-import com.ll.playon.domain.title.entity.QMemberTitle;
-import com.ll.playon.domain.title.entity.QTitle;
 import com.querydsl.core.types.dsl.CaseBuilder;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
@@ -18,32 +15,6 @@ import java.util.List;
 @RequiredArgsConstructor
 public class GuildMemberRepositoryCustomImpl implements GuildMemberRepositoryCustom {
     private final JPAQueryFactory queryFactory;
-
-    @Override
-    public List<GuildMember> findTopNByGuildOrderByRoleAndCreatedAt(Guild guild, int limit) {
-        QGuildMember gm = QGuildMember.guildMember;
-        QMember m = QMember.member;
-        QMemberTitle mt = QMemberTitle.memberTitle;
-        QTitle t = QTitle.title;
-
-        return queryFactory
-                .selectFrom(gm)
-                .leftJoin(gm.member, m).fetchJoin()
-                .leftJoin(m.memberTitles, mt).fetchJoin()
-                .leftJoin(mt.title, t).fetchJoin()
-                .where(gm.guild.eq(guild))
-                .orderBy(
-                        new CaseBuilder()
-                                .when(gm.guildRole.eq(GuildRole.LEADER)).then(0)
-                                .when(gm.guildRole.eq(GuildRole.MANAGER)).then(1)
-                                .otherwise(2)
-                                .asc(),
-                        gm.createdAt.desc()
-                )
-                .limit(limit)
-                .distinct()
-                .fetch();
-    }
 
     @Override
     public List<GuildMember> findTopNByGuildOrderByRoleAndCreatedAt(Guild guild, int limit) {
