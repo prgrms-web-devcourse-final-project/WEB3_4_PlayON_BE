8fa55e605cf5bfe1eb913a8a4842c6719c6ccfc5
Gyeonghun Min <riverpower6@naver.com>
2025-04-09 12:28:38 +0900

    [FIX] 파티 리스트 조회 HTTP 요청 방식 변경

--- src/main/java/com/ll/playon/domain/party/party/controller/PartyController.java
+++ src/main/java/com/ll/playon/domain/party/party/controller/PartyController.java
@@ -12,7 +12,6 @@ import com.ll.playon.domain.party.party.dto.response.GetPartyResponse;
 import com.ll.playon.domain.party.party.dto.response.GetPartyResultResponse;
 import com.ll.playon.domain.party.party.dto.response.PostPartyResponse;
 import com.ll.playon.domain.party.party.dto.response.PutPartyResponse;
-import com.ll.playon.domain.party.party.dto.response.*;
 import com.ll.playon.domain.party.party.service.PartyService;
 import com.ll.playon.domain.title.entity.enums.ConditionType;
 import com.ll.playon.domain.title.service.TitleEvaluator;
@@ -23,13 +22,21 @@ import com.ll.playon.standard.page.dto.PageDto;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import java.time.LocalDateTime;
 import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.format.annotation.DateTimeFormat.ISO;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
-
-import java.time.LocalDateTime;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
@@ -52,7 +59,7 @@ public class PartyController {
     }
 
     // TODO : 게임 쪽 나오면 게임 쪽 추가
-    @GetMapping
+    @PostMapping("/list")
     @Operation(summary = "조건 및 태그에 맞는 파티 리스트 조회")
     public RsData<PageDto<GetPartyResponse>> getAllParties(
             @RequestParam(defaultValue = "1") int page,
