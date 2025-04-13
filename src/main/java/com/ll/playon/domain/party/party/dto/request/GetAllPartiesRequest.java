12fd4dc2dfaa6aec83dad065213542b8fc39010f
Gyeonghun Min <riverpower6@naver.com>
2025-04-14 00:02:44 +0900

    [FIX] 파티 태그 파싱 오류 수정

--- src/main/java/com/ll/playon/domain/party/party/dto/request/GetAllPartiesRequest.java
+++ src/main/java/com/ll/playon/domain/party/party/dto/request/GetAllPartiesRequest.java
@@ -1,5 +1,6 @@
 package com.ll.playon.domain.party.party.dto.request;
 
+import com.ll.playon.global.type.TagValue;
 import jakarta.validation.Valid;
 import java.util.List;
 import org.springframework.util.CollectionUtils;
@@ -17,6 +18,6 @@ public record GetAllPartiesRequest(
     }
 
     public List<String> getTagValues() {
-        return tags != null ? tags.stream().map(PartyTagRequest::value).toList() : null;
+        return tags != null ? tags.stream().map(tag -> TagValue.fromValue(tag.value()).name()).toList() : null;
     }
 }
